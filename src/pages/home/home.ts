b1794ba13686d936ed69ae95fb4313a87806fb88
Suguru Seto <seto@trance-media.jp>
2018-03-16 15:13:42 +0900

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
+import { IonicPage, NavController, LoadingController } from 'ionic-angular';
 
+@IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	posts:{
+		ID: number,
+		title: string,
+		content: string,
+		date: string,
+	}[] = [];
 
-  }
+  	constructor(
+		  public navCtrl: NavController,
+		  public http: HttpClient,
+		  public loadingCtrl: LoadingController
+  	) {}
+
+  	ionViewDidLoad() {
+		  let loading = this.loadingCtrl.create();
+		  loading.present();
+		  this.http
+			.get('https://public-api.wordpress.com/rest/v1.1/sites/ionicjp.wordpress.com/posts')
+  			.subscribe(data => {
+				  this.posts = data['posts'];
+				  loading.dismiss();
+  			});
+  	}
 
 }
